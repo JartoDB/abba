95328eda5071d17302512d598735106f22c3f7d3
uesteibar <uesteibar@gmail.com>
2017-06-01 20:54:16 +0200

    Add webpack for js assets
    Closes #1

---
+++ frontend/js/components/DemoComponent/DemoComponent.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class DemoComponent extends Component {
+  render() {
+    return (
+      <div className="jumbotron text-center">
+      <h2>Welcome to Abba!</h2>
+      <h2>I'm being rendered from react!</h2>
+    </div>
+    )
+  }
+}
+
+export default DemoComponent
