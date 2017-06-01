36cfe53d99de1f6dc9d42fbd4c0fad6dcf320515
uesteibar <uesteibar@gmail.com>
2017-06-01 20:54:16 +0200

    Add webpack for js assets
    Closes #1

---
+++ frontend/js/app.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import DemoComponent from './components/DemoComponent'
+
+ReactDOM.render(<DemoComponent />, document.getElementById('react-demo'))
