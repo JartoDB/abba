14cb1fa46802f8cf3d87aafca1dea613b385ab32
uesteibar <uesteibar@gmail.com>
2017-06-13 09:58:44 +0200

    Add support for different bucket regions

--- lib/abba/aws/s3/bucket/server.ex
+++ lib/abba/aws/s3/bucket/server.ex
@@ -7,25 +7,26 @@ defmodule Abba.Aws.S3.Bucket.Server do
 
   def init(:ok) do
     bucket_name = System.get_env("AWS_S3_BUCKET_NAME")
+    bucket_host = System.get_env("AWS_S3_BUCKET_REGION")
 
-    {:ok, %{bucket_name: bucket_name}}
+    {:ok, %{bucket_name: bucket_name, bucket_host: bucket_host}}
   end
 
   def list_objects() do
     GenServer.call(:amazon_bucket_server, :list_objects)
   end
 
-  def handle_call(:list_objects, _from, %{bucket_name: bucket_name} = state) do
-    urls = fetch_objects(bucket_name)
-            |> extract_contents(bucket_name)
+  def handle_call(:list_objects, _from, %{bucket_name: bn, bucket_host: bh} = state) do
+    urls = fetch_objects(bn, bh)
+            |> extract_contents(bn)
 
     {:reply, urls, state}
   end
 
-  defp fetch_objects(bucket_name) do
+  defp fetch_objects(bucket_name, bucket_host) do
     bucket_name
       |> ExAws.S3.list_objects()
-      |> ExAws.request()
+      |> ExAws.request(region: bucket_host)
   end
 
   defp extract_contents({:ok, %{body: %{contents: contents }}}, bucket_name) do
